a79f121ebb4efaeb73195839c16ece8309e4bda5
piousbox <piousbox@gmail.com>
2017-10-08 20:56:37 -0700

    not using travis for this project

--- test/dummy/spec/spec_helper.rb
+++ test/dummy/spec/spec_helper.rb
@@ -1,5 +1,4 @@
 ENV["RAILS_ENV"] ||= 'test'
-# require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 require File.expand_path("../../config/environment.rb", __FILE__)
 require 'rspec/rails'
 require 'devise'
@@ -10,29 +9,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :helper
   config.include Devise::TestHelpers, :type => :controller
   config.include Devise::Test::ControllerHelpers, :type => :controller
-
 end
-
-=begin
-class UserStub
-  def initialize args = {}
-    @profile = OpenStruct.new
-    if args[:manager]
-      @profile[:manager?] = true
-    end
-  end
-
-  def profile= profile
-    @profile = profile
-  end
-
-  def profile
-    return @profile
-  end
-end
-=end
-
-# require_relative './factories'
 
 def puts! a, b=''
   puts "+++ +++ #{b}"
