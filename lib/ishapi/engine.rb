c0e564fbb33ad8c6228760b918580e88198ba65e
piousbox <piousbox@gmail.com>
2017-11-07 08:57:11 -0800

    no throttle for now

--- lib/ishapi/engine.rb
+++ lib/ishapi/engine.rb
@@ -3,6 +3,6 @@ require 'rack/throttle'
 module Ishapi
   class Engine < ::Rails::Engine
     isolate_namespace Ishapi
-    config.middleware.use Rack::Throttle::Interval, :min => 1.0, :max => 10
+    # config.middleware.use Rack::Throttle::Interval, :min => 1.0, :max => 10
   end
 end
